47df365d38cead56531c5a28048a295c0186c40e
Chen Wei-Chen <88455009+Double-T1@users.noreply.github.com>
2022-05-17 11:49:13 +0800

    Update solution.cpp

--- 1800-2000/1932. Merge BSTs to Create Single BST/solution.cpp
+++ 1800-2000/1932. Merge BSTs to Create Single BST/solution.cpp
@@ -1,3 +1,54 @@
+//solution 1 and solution 2 are basically the same idea
+//1 is with dfs and 2 is with bfs
+//however, 1 is much cleaner and simpler in terms of code
+//solution 1
+class Solution {
+public:
+    TreeNode* canMerge(vector<TreeNode*>& trees) {
+        unordered_map<int,TreeNode*> roots;
+        unordered_map<int,int> count;
+        for (auto node: trees) {
+            if (!toRegister(node,roots)) return nullptr;
+            count[node->val]++;
+            if (node->left) count[node->left->val]++;
+            if (node->right) count[node->right->val]++;
+        }
+        
+        TreeNode* root = nullptr;
+        for (auto&[val,num] : count) {
+            if (num == 1 && roots.count(val)) root = roots[val];
+        }
+        if (root == nullptr) return nullptr;
+        
+        roots.erase(root->val);
+        return merge(roots,root) && roots.empty() ? root : nullptr; 
+    }
+    
+private:
+    bool merge(unordered_map<int,TreeNode*>& m, TreeNode* r, int low=INT_MIN, int up=INT_MAX) {
+        if (r == nullptr) return true;
+        if (r->val <= low || r->val >= up) return false;
+        //both nullptr
+        if (r->left == r->right) {
+            auto it = m.find(r->val);
+            if (it != m.end() && it->second != r) {
+                r->left = it->second->left;
+                r->right = it->second->right;
+                m.erase(r->val);
+            }
+        }
+        return merge(m,r->left,low,r->val) && merge(m,r->right,r->val,up);
+    }
+    
+    bool toRegister(TreeNode* node, unordered_map<int,TreeNode*>& map) {
+        if (map.find(node->val) != map.end()) return false;
+        map[node->val] = node;
+        return true;
+    }
+};
+
+
+//solution 2
 class Solution {
 public:
     TreeNode* canMerge(vector<TreeNode*>& trees) {
