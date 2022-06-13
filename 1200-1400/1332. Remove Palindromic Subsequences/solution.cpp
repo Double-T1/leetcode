a9e008df80164c51dd15230f5e99006595ef6c3c
Chen Wei-Chen <88455009+Double-T1@users.noreply.github.com>
2022-06-13 16:45:02 +0800

    Create solution.cpp

---
+++ 1200-1400/1332. Remove Palindromic Subsequences/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int i = 0, j = s.size()-1;
+        while (i<j && s[i] == s[j]) {
+            i++; j--;
+        }
+        
+        return i >= j ? 1 : 2;
+    }
+};
+
