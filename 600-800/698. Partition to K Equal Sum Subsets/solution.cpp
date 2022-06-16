47798abbe089ce5e8215ecc3dac3970eaf681746
Chen Wei-Chen <88455009+Double-T1@users.noreply.github.com>
2022-06-16 16:32:51 +0800

    Update solution.cpp

--- 600-800/698. Partition to K Equal Sum Subsets/solution.cpp
+++ 600-800/698. Partition to K Equal Sum Subsets/solution.cpp
@@ -28,3 +28,40 @@ private:
         return false;
     };
 };
+
+//acutually becomes much faster without hashset
+class Solution {
+public:
+    int target;
+    vector<int> subsets;
+    vector<int> nums;
+    
+    bool canPartitionKSubsets(vector<int>& nums2, int k) {
+        int sum = accumulate(nums2.begin(),nums2.end(),0);
+        if (sum%k || nums2.size()<k) return false;
+        target = sum/k;
+        
+        nums = nums2;
+        subsets = vector<int>(k,0);
+        sort(nums.begin(),nums.end(),greater<int>());
+        return dfs(0);
+    }
+    
+private:
+    bool dfs(int i){
+        if (i == nums.size()) return true;
+
+        //unordered_set<int> seen;
+        for (int j=0; j<subsets.size(); j++) {
+            // if (seen.find(subsets[j]) != seen.end()) continue;
+            // seen.insert(subsets[j]);
+            if (nums[i] + subsets[j] <= target) {
+                subsets[j] += nums[i];
+                if (dfs(i+1)) return true;
+                subsets[j] -= nums[i];
+            }
+            if (subsets[j] == 0) return false;
+        }
+        return false;
+    };
+};
